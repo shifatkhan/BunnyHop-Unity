b348717f3ec5c3a72641c33c397048519d5b92c7
Shifat Khan <shifatkhan6@gmail.com>
2017-05-21 19:59:41 -0400

    Added physics

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+
+    private Rigidbody2D bunnyRigidBody;
+    public float bunnyJumpForce = 200f;
+
+	// Use this for initialization
+	void Start () {
+        // Get reference for RigitBody2D
+        bunnyRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonUp("Jump"))
+        {
+            bunnyRigidBody.AddForce(transform.up * bunnyJumpForce);
+        }
+	}
+}
